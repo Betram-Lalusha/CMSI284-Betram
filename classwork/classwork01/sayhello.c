ebf7f41cd4c102294f1b3845549b96b14a0071d9
Betram Lalusha <lalushabetm@gmail.com>
2022-08-30 23:19:21 -0700

    submission for classwork01 c code

--- classwork/classwork01/sayhello.c
+++ classwork/classwork01/sayhello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	printf("\n\nHello world!\n\n");
-
-	return 0;
+	char enteredName[10]; 
+	printf("please enter your name: \n");
+	printf("\n");
+    fgets(enteredName, 10, stdin);
+    printf("\n");
+    printf("    ");
+    printf("Hello! %s ", enteredName);
+    printf("\n");
 }
