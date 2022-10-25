f10a39087779200be01afb3432451c784187379e
David Yang <davidyang@Davids-MacBook-Pro.local>
2022-10-25 15:23:02 -0700

    timesTables

---
+++ homework/homework05/timesTables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+   int j,i,n;
+   printf("Input number to display times tables up to \n");
+   scanf("%d",&n);
+   printf("Multiplication table from 2 to %d \n",n);
+   for(i=2;i<=10;i++)
+   {
+     for(j=2;j<=n;j++)
+     {
+	    printf("%d * %d = %d, ",j,i,i*j);
+      }
+     printf("\n");
+    }
+}
