12761dfdfef48b31801a34d6e31d8fb334008d70
Betram Lalusha <lalushabetm@gmail.com>
2022-11-17 15:00:12 -0800

    adding say hi

---
+++ classwork/classwork11/sayHiTimes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+void sayHiTimes(uint64_t num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        printf("Hi");
+    }
+
+    printf("\n");
+    printf("\n");
+}
---
+++ classwork/classwork11/sayHiTimesTest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+void sayHiTimes(uint64_t num);
+
+int main(int argc, char *argv[])
+{
+    sayHiTimes(4);
+    sayHiTimes(5);
+    sayHiTimes(1);
+
+    return 1;
+}
